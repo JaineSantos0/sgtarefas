68efa4469c6e0036710bdfe32ba4e4c126f29313
Jaine Santos <jainejosiane@gmail.com>
2024-09-08 15:38:44 -0300

    refactor: moving to application package

--- src/main/java/com/jainejosiane/sgtarefas/application/SgtarefasApplication.java
+++ src/main/java/com/jainejosiane/sgtarefas/application/SgtarefasApplication.java
@@ -1,8 +1,10 @@
-package com.jainejosiane.sgtarefas;
+package com.jainejosiane.sgtarefas.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"com.jainejosiane.sgtarefas"})
 @SpringBootApplication
 public class SgtarefasApplication {
 
