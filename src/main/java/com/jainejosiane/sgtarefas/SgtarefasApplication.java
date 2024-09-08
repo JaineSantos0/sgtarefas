e42d249ab94c3f3e762c855bda2304015e616f8a
Jaine Santos <jainejosiane@gmail.com>
2024-09-08 14:36:23 -0300

    initial commit

---
+++ src/main/java/com/jainejosiane/sgtarefas/SgtarefasApplication.java
@@ -0,0 +1,13 @@
+package com.jainejosiane.sgtarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgtarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgtarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jainejosiane/sgtarefas/SgtarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jainejosiane.sgtarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgtarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
