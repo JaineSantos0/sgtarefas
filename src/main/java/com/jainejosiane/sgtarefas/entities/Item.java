383fdc207642e74f6d879adb023953a191d5ae53
Jaine Santos <jainejosiane@gmail.com>
2024-09-09 23:57:02 -0300

    feat: adding business rule

--- src/main/java/com/jainejosiane/sgtarefas/entities/Item.java
+++ src/main/java/com/jainejosiane/sgtarefas/entities/Item.java
@@ -1,6 +1,9 @@
 package com.jainejosiane.sgtarefas.entities;
 
+import com.jainejosiane.sgtarefas.dto.ItemDTO;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -10,22 +13,35 @@ import lombok.*;
 @AllArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Item {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(min = 5, max = 20)
+    @NotBlank(message = "Title is required")
+    @Size(min = 5, max = 20, message = "Title must have a minimum of 5 characters and a maximum of 20")
     private String title;
 
+    @Column(columnDefinition = "TEXT")
     private String description;
 
+    @NotNull
     private ItemStatus status;
 
+    @NotNull
     private Boolean highlighted;
 
     @ManyToOne
     @JoinColumn(name = "task_id")
     private Task task;
+
+    public Item(ItemDTO dto) {
+        id = dto.getId();
+        title = dto.getTitle();
+        description = dto.getDescription();
+        status = dto.getStatus();
+        highlighted = dto.getHighlighted();
+    }
 }
--- src/main/java/com/jainejosiane/sgtarefas/entities/Task.java
+++ src/main/java/com/jainejosiane/sgtarefas/entities/Task.java
@@ -1,6 +1,7 @@
 package com.jainejosiane.sgtarefas.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -11,18 +12,29 @@ import java.util.List;
 @Table(name = "tb_task")
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 @Getter
-@Setter()
+@Setter
 public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size(min = 5, max = 20)
+    @NotBlank(message = "Title is required")
+    @Size(min = 5, max = 20, message = "Title must have a minimum of 5 characters and a maximum of 20")
     private String title;
 
-    @Setter(AccessLevel.NONE)
-    @OneToMany(mappedBy = "task")
+    @OneToMany(mappedBy = "task",cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Item> itemList = new ArrayList<>();
+
+    public void addItem(Item item) {
+        this.itemList.add(item);
+        item.setTask(this);
+    }
+
+    public void removeItem(Item item) {
+        this.itemList.remove(item);
+        item.setTask(null);
+    }
 }
